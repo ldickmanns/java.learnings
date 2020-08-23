3a96070ea7ae0abec8662baa777f9963b8f93cfe
Ludwig Dickmanns <ludwig.dickmanns@outlook.de>
2020-08-23 09:44:32 +0200

    Post 092: Rounding

--- src/math/general/Rounding.java
+++ src/math/general/Rounding.java
@@ -1,26 +1,21 @@
 package math.general;
 
-/*******************
- * Java Learnings  *
- * @java.learnings *
- *******************/
+/********************
+ * Java Learnings   *
+ * @java.learnings  *
+ ********************/
 
 public class Rounding {
+    public static void main(String[] args) {
+        double d = 1.5;
 
-	public static void main(String[] args) {
-		System.out.println(Math.round(0.0));
-		System.out.println(Math.round(0.3));
-		System.out.println(Math.round(0.5));
-		System.out.println(Math.round(0.7));
-		System.out.println(Math.round(1.0));
-	}
+        double floor = Math.floor(d);
+        double ceil = Math.ceil(d);
+        double round = Math.round(d);
+
+        System.out.println(floor);  /** 1.0 */
+        System.out.println(ceil);   /** 2.0 */
+        System.out.println(round);  /** 2.0 */
+    }
 }
 
-/************
- * Output:	*
- * 0		*
- * 0		*
- * 1		*
- * 1		*
- * 1		*
- ************/
---
+++ src/math/general/RoundingOld.java
@@ -0,0 +1,26 @@
+package math.general;
+
+/*******************
+ * Java Learnings  *
+ * @java.learnings *
+ *******************/
+
+public class RoundingOld {
+
+	public static void main(String[] args) {
+		System.out.println(Math.round(0.0));
+		System.out.println(Math.round(0.3));
+		System.out.println(Math.round(0.5));
+		System.out.println(Math.round(0.7));
+		System.out.println(Math.round(1.0));
+	}
+}
+
+/************
+ * Output:	*
+ * 0		*
+ * 0		*
+ * 1		*
+ * 1		*
+ * 1		*
+ ************/
